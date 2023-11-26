5454a470f7c7c83f8729b21d582a63dd70cf6808
Никита Новиков <NovikovNS7@outlook.com>
2023-11-26 23:18:57 +0500

    Task5

---
+++ Tyuiu.NovikovNS.Sprint5.Task5.V22/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using Tyuiu.NovikovNS.Sprint5.Task5.V22.Lib;
+
+namespace Tyuiu.NovikovNS.Sprint5.Task5.V22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт#5 | Выполнил: Новиков Н.С. | ИСТНб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт#5                                                                *");
+            Console.WriteLine("* Тема: Чтение набора данных из текстового файла                          *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #22                                                             *");
+            Console.WriteLine("* Выполнил: Новиков Никита Сергеевич | ИСТНб-23-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан файл С:DataSprint5InPutDataFileTask5V22.txt (файл взять из архива   *");
+            Console.WriteLine("* согласно вашему варианту. Создать папку в ручную С:DataSprint5 и        *");
+            Console.WriteLine("* скопировать в неё файл) в котором есть набор значений. Найти минимальное*");
+            Console.WriteLine("* целое число в файле, которое делится на 4. Полученный результат вывести *");
+            Console.WriteLine("* на консоль. У вещественных значений округлить до трёх знаков после ,    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string path = $@"C:\DataSprint5\InPutDataFileTask5V22.txt";
+            Console.WriteLine($"Данные находятся в файле: {path}");
+
+
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
